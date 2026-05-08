d527b7f9fa2037cc62a8243fc233dbb14f70d984
Camila Ines Fernandez Garrido <camifernandezgarrido2@gmail.com>
2026-05-08 20:00:13 -0300

    chore: fix folder issues

--- apps/web/app/postulation/[jobId]/page.tsx
+++ apps/web/app/postulation/[jobId]/page.tsx
@@ -1,4 +1,4 @@
-import PostulationView from "@/app/features/postulation/components/PostulationView";
+import PostulationView from "@/features/postulation/components/PostulationView";
 
 export default function PostulationPage() {
   return <PostulationView />;
